7e86de3a14a93c5be05a277a8891bae92fa2faf7
ana <107580142+anapagn0@users.noreply.github.com>
2024-11-08 09:44:30 -0300

    08/11

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,9 @@
+const nomes =["Alex de Morais", "Edgar", "Marselão", "Lula", " Mitinho", "Babidi(que Deus o tenha)"];
+
+
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes)
